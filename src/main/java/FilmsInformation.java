2a1969e361dd4c379c97c879fcef37076615ae6f
Marina Iurchenko <lamanch1203@gmail.com>
2023-05-16 13:48:13 +0300

    initial commit

---
+++ src/main/java/FilmsInformation.java
@@ -0,0 +1,35 @@
+public class FilmsInformation {
+    private int id;
+    private String name;
+    private String genre;
+
+    public FilmsInformation(int id,String name,String genre){
+        this.id = id;
+        this.name = name;
+        this.genre = genre;
+    }
+    public int getId(){
+
+        return id;
+    }
+    public void setId(int id){
+
+        this.id = id;
+    }
+    public String getName(){
+
+        return name;
+    }
+    public void setName(String name){
+
+        this.name = name;
+    }
+    public String getGenre(){
+
+        return genre;
+    }
+    public void setGenre(String genre){
+
+        this.genre = genre;
+    }
+}
